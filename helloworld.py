463bda21aaa328f7fc5479dfd102790787e9a3ad
Alberto Iong <albert0i@hotmail.com>
2025-01-15 09:19:09 +0800

    ignore *.pyc

--- helloworld.py
+++ helloworld.py
@@ -3,7 +3,7 @@ from redis import Redis
 import os
 
 HOST = os.environ.get("REDIS_HOST", "localhost")
-PORT = os.environ.get("REDIS_PORT", 6379)
+PORT = os.environ.get("REDIS_PORT", 7000)
 USERNAME = os.environ.get("REDIS_USER")
 PASSWORD = os.environ.get("REDIS_PASSWORD")
 
